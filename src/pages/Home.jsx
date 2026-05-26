77447666286dabb058da05e47ca9fdfd7e4dd15a
melisb3@gmail.com <melisb3@gmail.com>
2026-05-26 12:39:40 +0200

    UI improvements + CSS refactor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,33 @@
 import { Link } from "react-router-dom";
 import "./General.css";
 import "./Home.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+
+
+const images = [
+  "/cuteCarPics/cuteCar1.png",
+  "/cuteCarPics/cuteCar2.png",
+  "/cuteCarPics/cuteCar3.png",
+  "/cuteCarPics/cuteCar4.png",
+  "/cuteCarPics/cuteCar5.png",
+  "/cuteCarPics/cuteCar6.png",
+  "/cuteCarPics/cuteCar7.png",
+  "/cuteCarPics/cuteCar8.png",
+  "/cuteCarPics/cuteCar9.png",
+  "/cuteCarPics/cuteCar10.png",
+  "/cuteCarPics/cuteCar11.png",
+  "/cuteCarPics/cuteCar12.png",
+  "/cuteCarPics/cuteCar13.png",
+  "/cuteCarPics/cuteCar14.png",
+  "/cuteCarPics/cuteCar15.png",
+  "/cuteCarPics/cuteCar16.png",
+  "/cuteCarPics/cuteCar17.png",
+  "/cuteCarPics/cuteCar18.png",
+  "/cuteCarPics/cuteCar19.png",
+  "/cuteCarPics/cuteCar20.png"
+];
 
 function Home() {
 
@@ -25,6 +52,34 @@ function Home() {
         </Link>
 
       </div>
+
+      
+        <Swiper
+            modules={[Autoplay]}
+              autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+              }}
+            spaceBetween={20}
+            slidesPerView={1}
+            loop={true}
+            className="mySwiper"
+          >
+           
+            {images.map((img, index) => (
+              <SwiperSlide key={index}>
+                <div className="slideContainer">
+                <img
+                  src={img}
+                  alt="vehicle"
+                  className="slideImage"
+              />
+              </div>
+              </SwiperSlide>
+            ))}
+            
+        </Swiper>
+      
 
     </div>
   );
